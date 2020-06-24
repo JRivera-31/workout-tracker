904d1f96df4bd997fc1e5103f989b1305cb95eca
Josh R <jriver35@uncc.edu>
2020-06-23 22:00:12 -0400

    Added get route to retrieve all workouts from db

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require("express")
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 const logger = require("morgan")
 const app = express();
@@ -12,10 +18,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-const htmlRoutes = require("./routes/html-routes.js")
-htmlRoutes(app)
-const apiRoutes = require("./routes/api-routes.js")
-apiRoutes(app)
+require("./routes/html-routes.js")(app)
+require("./routes/api-routes.js")(app)
+
 
 
 app.listen(PORT, () => console.log("🌎 Listening on " + PORT))
