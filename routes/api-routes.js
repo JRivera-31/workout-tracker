6abb39b051aa700d43a475af2fa8b8d34dcab622
Josh R <jriver35@uncc.edu>
2020-06-23 22:17:48 -0400

    added post route and get by id route, currently not working

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -9,4 +9,18 @@ module.exports = function (app) {
             })
             .catch(err => res.json(err))
     })
+
+    app.post("/api/workouts", ({ body }, res) => {
+        db.Workout.create(body)
+            .then(newWorkout => {
+                res.json(newWorkout)
+            })
+            .catch(err => res.json(err))
+    })
+
+    app.get("/api/workouts/:id", (req, res) => {
+        db.Workout.findById({ id: req.params.id })
+            .then(workout => res.json(workout))
+            .catch(err => res.json(err))
+    })
 }
